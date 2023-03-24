76efbe4d300464d190e6a345348049ed6a732e13
John Doe <jiangwudid0109@gmail.com>
2023-03-24 19:52:16 +0900

    3/24

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import  {useState} from 'react';
+import './App.css';
+
+const initialvalues = {username : "", mailaddress : "",  password : ""};
+function App() {
+  const [A,setA] = useState(initialvalues);
+  return (
+   <div>
+    <h1>ログインフォーム</h1>
+    <hr></hr>
+    <div>
+    <label>username</label>
+      <input
+      type = "text"
+      name = "username"
+      placeholder = "username"></input>
+      <br></br>
+        <label>username</label>
+      <input
+      type = "text"
+      name = "username"
+      placeholder = "username"></input>
+      <br></br>
+      <label>username</label>
+      <input
+      type = "text"
+      name = "username"
+      placeholder = "username"></input>
+      <br></br>
+      <button>login</button>
+    </div>
+   </div>
+  );
+}
+
+export default App;
